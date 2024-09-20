5dc5906a989fca904426ee570e75dd8e744bdd22
Titan <68808479+titan3755@users.noreply.github.com>
2024-09-20 13:13:56 +0600

    Added ping-all basic functionality

--- ping/ping_all.go
+++ ping/ping_all.go
@@ -2,7 +2,12 @@ package ping
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"netzer/utils"
+	"strings"
+	"sync"
+	"time"
 	"github.com/pterm/pterm"
 	"github.com/urfave/cli/v2"
 )
@@ -26,9 +31,47 @@ func PingAllMain(c *cli.Context) error {
 		pterm.Info.Println(fmt.Sprintf("%v. %v \n", no+1, ip))
 	}
 	pterm.Info.Println("Pinging IPs ...")
+	gor_ip_latency := make(chan string)
+	wg := &sync.WaitGroup{}
+	for _, ip := range ip_list {
+		wg.Add(1)
+		go func(ip string) {
+			defer wg.Done()
+			var latency string = ""
+			startTime := time.Now()
+			conn, err := net.Dial("tcp", ip)
+			if err != nil {
+				latency = fmt.Sprintf("Error: %v", err)
+				gor_ip_latency <- latency
+			}
+			go func() {
+				time.Sleep(2 * time.Second)
+				if latency == "" || !strings.Contains(latency, "Error") {
+					latency = "Error: Timeout"
+					gor_ip_latency <- latency
+					wg.Done()
+				}
+			}()
+			defer conn.Close()
+			latency = fmt.Sprintf("%v", time.Since(startTime))
+			log.Default().Printf("Latency: %s, IP: %s\n", latency, ip)
+			gor_ip_latency <- latency
+		}(ip)
+	}
+	go func() {
+		wg.Wait()
+		close(gor_ip_latency)
+	}()
+	for latency := range gor_ip_latency {
+		if strings.Contains(latency, "Error") {
+			pterm.Error.Println(fmt.Sprintf("Error: %s", latency))
+		} else {
+			pterm.Success.Println(fmt.Sprintf("Latency: %s", latency))
+		}
+	}
 	return nil
 }
 
-func pingAll() {
+// func pingAll() {
 	
-}
+// }
---
+++ utils/icmp_ping.go
@@ -0,0 +1,11 @@
+package utils
+
+// import (
+// 	"fmt"
+// 	"net"
+// 	"os"
+// 	"time"
+// )
+
+// ICMP_Ping is a function that sends an ICMP echo request to the specified host
+
